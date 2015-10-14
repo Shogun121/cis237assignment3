ec53abdabd5333b0dffb3405159a2c0188593a6e
Robert Cooley <Daimyo121@aol.com>
2015-10-14 15:49:50 -0400

    Creation + Update of Astromech -subclass of Utility.

---
+++ cis237assignment3/Astromech.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cis237assignment3
+{
+    class Astromech:Utility
+    {
+        //backing field
+        protected bool fireExtinguisher;
+        protected int numberShips;
+
+        protected const decimal fireExintinguisher_Cost = 100m;
+        protected const decimal cost_per_Ship = 40m;
+
+        //Constructor
+        public Astromech(string Material, string Model, string Color, bool Toolbox, bool ComputerConnection, bool Arm,bool FireExtinguisher,int NumberShips)
+            : base(Material,Model,Color,Toolbox,ComputerConnection,Arm)
+        {
+            this.fireExtinguisher = FireExtinguisher;
+            this.numberShips = NumberShips;
+        }
+
+        //Properties
+
+        //Methods
+        public override string ToString()
+        {
+            return base.ToString() + " FireExtinguisher: " + fireExtinguisher + " Number of Ships: " + numberShips;
+        }
+        public override void CalculateTotalCost()
+        {
+            if(fireExtinguisher)
+            {
+                totalCost += fireExintinguisher_Cost;
+            }
+            if(numberShips<0)
+            {
+                totalCost += (cost_per_Ship * numberShips);
+            }
+        }
+    }
+}
