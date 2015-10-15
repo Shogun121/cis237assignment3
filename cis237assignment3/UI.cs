8475afaa644b0f168c03e41f8566a507fd2364e2
Robert Cooley <Daimyo121@aol.com>
2015-10-15 13:02:06 -0400

    Update to UI class 1:02pm 10/15

--- cis237assignment3/UI.cs
+++ cis237assignment3/UI.cs
@@ -10,17 +10,51 @@ namespace cis237assignment3
     {
         //Backing fields
         private bool exit=false;
+        private int userChoice;
+        private const int menuOptions=2;
         //Constructor
 
         //Properties
 
         //Methods
-        public void RunLoop()
+        public void MenuLoop()
         {
             while(exit==false)
             {
-
+                Console.WriteLine("1)......Add a Droid.");
+                Console.WriteLine("2)......Print Driod list.");
             }
+        }
+        public void Error()
+        {
+            Console.WriteLine("Input is invalid");  //display if input is invalid.
+        }
+        public void PromptUser()
+        {
+            Console.WriteLine();
+            Console.Write("Make a selection: ");
+        }
+        public string GetResponse()
+        {
+            return Console.ReadLine();
+        }
+        //Verify input integrity
+        public bool VerifyInput(string input)
+        {
+            bool VerifiedInput=false;
+            try
+            {
+                userChoice=Int32.Parse(input);
+                if(userChoice>0 && userChoice<menuOptions)
+                {
+                    VerifiedInput = true;
+                }
+            }
+            catch(Exception ex)
+            {
+                VerifiedInput = false;
+            }
+            return VerifiedInput;
         }
     }
 }
