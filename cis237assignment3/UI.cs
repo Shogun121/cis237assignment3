d65959ce190c54776e33efa11ac52ed74320f875
Robert Cooley <Daimyo121@aol.com>
2015-10-15 12:32:28 -0400

    UI class created.updated 12:32pm 10/15

---
+++ cis237assignment3/UI.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cis237assignment3
+{
+    class UI
+    {
+        //Backing fields
+
+        //Constructor
+
+        //Properties
+
+        //Methods
+    }
+}
